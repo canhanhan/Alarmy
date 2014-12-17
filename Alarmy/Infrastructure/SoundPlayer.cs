540d0e2e0e572dc2d9018fa4fa8294d250685c79
Can Hanhan <p_github@finarfin.net>
2014-12-17 01:50:01 +0100

    Missing files from previous commit

---
+++ Alarmy/Infrastructure/SoundPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Alarmy.Infrastructure
+{
+    class SoundPlayer : IDisposable
+    {
+        private readonly System.Media.SoundPlayer soundPlayer;
+
+        public bool IsPlaying { get; private set; }
+        public string Path { get; private set; }
+
+        public SoundPlayer(string path)
+        {
+            this.Path = path;
+            this.soundPlayer = new System.Media.SoundPlayer(path);
+        }
+
+        public void Play()
+        {
+            if (this.IsPlaying)
+                return;
+            this.IsPlaying = true;
+
+            if (!this.soundPlayer.IsLoadCompleted)
+                this.soundPlayer.Load();
+
+            this.soundPlayer.PlayLooping();
+        }
+
+        public void Stop()
+        {
+            this.IsPlaying = false;
+            this.soundPlayer.Stop();
+        }
+
+        public void Dispose()
+        {
+            if (this.soundPlayer != null)
+            {
+                this.soundPlayer.Dispose();
+            }
+        }
+    }
+}
